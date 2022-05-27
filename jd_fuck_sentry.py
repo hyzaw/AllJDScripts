14669e76119036ea64a2adc8bcc994d68b4003da
清纯甜美女大学生 <hydsehuhfgbe@fwefwe.com>
2022-05-27 16:55:31 +0800

    AUTO SYNC t=1653641731

--- jd_fuck_sentry.py
+++ jd_fuck_sentry.py
@@ -1,9 +1,30 @@
 with open('/etc/hosts', 'a+') as f:
-    hosts = f.read()
-    if '127.0.0.1 o1098464.ingest.sentry.io' in hosts:
+    if 'sentry.io' in open('/etc/hosts').read():
         print('已屏蔽')
     else:
         f.write('\n127.0.0.1 o1098464.ingest.sentry.io\n')
         print('屏蔽成功')
 
 print(open('/etc/hosts').read())
+
+import os
+
+files = os.listdir('/ql/dist')
+filesraw = os.listdir('/ql/dist')
+for i in filesraw:
+    if os.path.isdir('/ql/dist/' + i) or '.gz' in i:
+        files.remove(i)
+js_sec = []
+for i in files:
+    with open('/ql/dist/' + i, 'r', encoding='utf-8') as f:
+        if 'sentry.io' in f.read():
+            print(i, '文件中含有sentry.io')
+            js_sec.append(i)
+for i in js_sec:
+    f = open('/ql/dist/' + i, 'r', encoding='utf-8')
+    file_content = f.read()
+    f.close()
+    f = open('/ql/dist/' + i, 'w', encoding='utf-8')
+    f.write(file_content.replace('sentry.io', ''))
+    f.close()
+    print('屏蔽js成功', i)
