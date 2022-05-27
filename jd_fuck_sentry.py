1143c1ccef565a68d9ae2c9d144d2df7ecf8e445
清纯甜美女大学生 <hydsehuhfgbe@fwefwe.com>
2022-05-27 16:12:46 +0800

    AUTO SYNC t=1653639166

---
+++ jd_fuck_sentry.py
@@ -0,0 +1,8 @@
+with open('/etc/hosts') as f:
+    hosts=f.read()
+    if '127.0.0.1 o1098464.ingest.sentry.io' in f:
+        print('已屏蔽')
+    else:
+        f.write('\n127.0.0.1 o1098464.ingest.sentry.io\n')
+        print('屏蔽成功')
+        
