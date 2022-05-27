f82c8d71494e11d3c78dbad73a76da1c731253d0
清纯甜美女大学生 <hydsehuhfgbe@fwefwe.com>
2022-05-27 16:19:12 +0800

    AUTO SYNC t=1653639552

--- jd_fuck_sentry.py
+++ jd_fuck_sentry.py
@@ -1,6 +1,6 @@
-with open('/etc/hosts','a+') as f:
-    hosts=f.read()
-    if '127.0.0.1 o1098464.ingest.sentry.io' in f:
+with open('/etc/hosts', 'a+') as f:
+    hosts = f.read()
+    if '127.0.0.1 o1098464.ingest.sentry.io' in hosts:
         print('已屏蔽')
     else:
         f.write('\n127.0.0.1 o1098464.ingest.sentry.io\n')
