17895d50f73e3e77c8d0f4c22eb1b2fd863a107d
清纯甜美女大学生 <hydsehuhfgbe@fwefwe.com>
2022-05-27 17:15:39 +0800

    AUTO SYNC t=1653642939

--- jd_fuck_sentry.py
+++ jd_fuck_sentry.py
@@ -9,22 +9,27 @@ print(open('/etc/hosts').read())
 
 import os
 
-files = os.listdir('/ql/dist')
-filesraw = os.listdir('/ql/dist')
-for i in filesraw:
-    if os.path.isdir('/ql/dist/' + i) or '.gz' in i:
-        files.remove(i)
-js_sec = []
-for i in files:
-    with open('/ql/dist/' + i, 'r', encoding='utf-8') as f:
-        if 'sentry.io' in f.read():
-            print(i, '文件中含有sentry.io')
-            js_sec.append(i)
-for i in js_sec:
-    f = open('/ql/dist/' + i, 'r', encoding='utf-8')
-    file_content = f.read()
-    f.close()
-    f = open('/ql/dist/' + i, 'w', encoding='utf-8')
-    f.write(file_content.replace('sentry.io', ''))
-    f.close()
-    print('屏蔽js成功', i)
+ql_dists = ['/ql/dist', '/ql/static/dist']
+for dist in ql_dists:
+    try:
+        files = os.listdir(dist)
+        filesraw = os.listdir(dist)
+        for i in filesraw:
+            if os.path.isdir(dist + '/' + i) or '.gz' in i:
+                files.remove(i)
+        js_sec = []
+        for i in files:
+            with open(dist + '/' + i, 'r', encoding='utf-8') as f:
+                if 'sentry.io' in f.read():
+                    print(i, '文件中含有sentry.io')
+                    js_sec.append(i)
+        for i in js_sec:
+            f = open(dist + '/' + i, 'r', encoding='utf-8')
+            file_content = f.read()
+            f.close()
+            f = open(dist + '/' + i, 'w', encoding='utf-8')
+            f.write(file_content.replace('sentry.io', ''))
+            f.close()
+            print('屏蔽js成功', i)
+    except:
+        pass
