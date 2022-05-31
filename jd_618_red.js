65d860119dad59a143288a6992dc54ea5f29cd5d
清纯甜美女大学生 <hydsehuhfgbe@fwefwe.com>
2022-05-31 21:51:51 +0800

    AUTO SYNC t=1654005111

--- jd_618_red.js
+++ jd_618_red.js
@@ -50,8 +50,8 @@ let appId, fingerprint, token, enCryptMethodJD;
     }
 })().catch((e) => { $.log('', `❌ ${$.name}, 失败! 原因: ${e}!`, '') }).finally(() => { $.done(); })
 
-async function main(ck, code = 'Jt4KpgH') {
-    const codes = ['Jt4KpgH','lMCi96W']
+async function main(ck, code = 'JKhfec3') {
+    const codes = ['JKhfec3']
     code = $.CODE618 ? $.CODE618 : codes[random(0, codes.length)]
     //console.log(code)
     let userName = decodeURIComponent(ck.match(/pt_pin=([^; ]+)(?=;?)/) && ck.match(/pt_pin=([^; ]+)(?=;?)/)[1])
