17db977c19f0966231e6704b3e91e03b002b0777
hyzaw <2595133804wls@gmail.com>
2022-11-12 18:01:33 +0800

    AUTO SYNC t=1668247293

--- jd_supergz.js
+++ jd_supergz.js
@@ -1,11 +1,14 @@
 /*
 一次性脚本
-6 8 * * * jd_supergz.js, tag=频道关注
+频道关注
+cron "11 21 25 10 *" jd_supergz.js
  */
 
 const $ = new Env('频道关注');
 const notify = $.isNode() ? require('./sendNotify') : '';
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
+
+
 let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭通知推送
 let cookiesArr = [], cookie = '', message;
 
